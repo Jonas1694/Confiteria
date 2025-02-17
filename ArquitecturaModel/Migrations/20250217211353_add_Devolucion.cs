555412dca0bc0fe08c1d4acd9f1395404b8b84bc
Willians Melendez <willian_melendez@hotmail.com>
2025-02-17 17:17:20 -0400

    Update

---
+++ ArquitecturaModel/Migrations/20250217211353_add_Devolucion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArquitecturaModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_Devolucion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Tasa",
+                table: "Devolucions",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tasa",
+                table: "Devolucions");
+        }
+    }
+}

129f35aba44a103712b240d0598733071181874f
Willians Melendez <Willians Melendez@DESKTOP-6NJ49DB>
2025-09-20 21:05:32 -0400

    Update

---
+++ ArquitecturaModel/Model/UserDto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquitecturaModel.Model
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Requerido")]
+        [Display(Name = "Nombre  ")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Campo Requerido")]
+        [Display(Name = "Apellido ")]
+        public string Apellido { get; set; }
+
+        //[Required(ErrorMessage = "Campo Requerido")]
+        [Display(Name = "Direccion  ")]
+        public string Direccion { get; set; }
+
+        //[Required(ErrorMessage = "Campo Requerido")]
+        [Display(Name = "Telefono  ")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "Campo Requerido")]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo Requerido")]
+        [StringLength(100, ErrorMessage = "El {0} debe tener al menos {2} y un máximo de {1} caracteres de longitud.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "La contraseña y la contraseña de confirmación no coinciden")]
+        public string ConfirmPassword { get; set; }
+
+        public int SucursalId { get; set; }
+    }
+}
---
+++ ArquitecturaModel/Services/UserService.cs
@@ -0,0 +1,103 @@
+﻿using ArquitecturaModel.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquitecturaModel.Services
+{
+    internal class UserService
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserStore<ApplicationUser> _userStore;
+        public UserService(IUserStore<ApplicationUser> userStore, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userStore = userStore;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<List<UserDto>> GetUsers()
+        {
+            var users = await _userManager.Users
+                .Select(user => new UserDto
+            {
+                Id = user.Id,
+                Nombre = user.Nombre,
+                Apellido = user.Apellido,
+                Direccion = user.Direccion,
+                Telefono = user.Telefono,
+                Email = user.Email,
+                SucursalId = user.SucursalId
+            }).ToListAsync();
+            return users;
+        }
+
+        public async Task<bool> CreateUserAsync(UserDto Input)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = Input.Email,
+                Email = Input.Email,
+                Nombre = Input.Nombre,
+                Apellido = Input.Apellido,
+                Direccion = Input.Direccion,
+                Telefono = Input.Telefono,
+                ModifyByUserId = Input.Email,
+                ModifyDescription = Input.Email,
+                SucursalId = Input.SucursalId
+            };
+            await _userStore.SetUserNameAsync(user, user.UserName, CancellationToken.None);
+            var result = await _userManager.CreateAsync(user, Input.Password);
+            return result.Succeeded;
+        }
+        public async Task<bool> DeleteUserAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return false;
+            }
+            var result = await _userManager.DeleteAsync(user);
+            return result.Succeeded;
+        }
+        public async Task<bool> UpdateUserAsync(UserDto Input)
+        {
+            var user = await _userManager.Users.Where(w=> w.Id == Input.Id).FirstOrDefaultAsync();
+            if (user == null)
+            {
+                return false;
+            }
+            user.Nombre = Input.Nombre;
+            user.Apellido = Input.Apellido;
+            user.Direccion = Input.Direccion;
+            user.Telefono = Input.Telefono;
+            user.Email = Input.Email;
+            user.UserName = Input.Email;
+            user.SucursalId = Input.SucursalId;
+            var result = await _userManager.UpdateAsync(user);
+            return result.Succeeded;
+        }
+
+        public async Task<UserDto?> GetUserByIdAsync(Guid userId)
+        {
+            var user = await _userManager.Users
+                .Where(w => w.Id == userId)
+                .Select(user => new UserDto
+                {
+                    Id = user.Id,
+                    Nombre = user.Nombre,
+                    Apellido = user.Apellido,
+                    Direccion = user.Direccion,
+                    Telefono = user.Telefono,
+                    Email = user.Email,
+                    SucursalId = user.SucursalId
+                }).FirstOrDefaultAsync();
+            return user;
+        }
+    }
+}

a162df6fa1c37b439902edf5ba9b0eca67607e2d
jonathan carrillo <jhonatancarrillo78@gmail.com>
2026-03-04 14:58:54 -0400

    creacion de precios 2

---
+++ ArquitecturaModel/Migrations/20260304172334_preciosvarios.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArquitecturaModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class preciosvarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Precio2",
+                table: "Productos",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PrecioDolar2",
+                table: "Productos",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Precio2",
+                table: "Productos");
+
+            migrationBuilder.DropColumn(
+                name: "PrecioDolar2",
+                table: "Productos");
+        }
+    }
+}
